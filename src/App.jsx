01735b57797d34ef2a896602931fb33730f55aec
yusufefedemiroglu <yusufefedemiroglu1@hotmail.com>
2025-06-22 03:55:56 +0300

    FiX commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from "react"
+import React from "react";
 import Hero from "./components/Hero";
-import About from "./components/About"
-import Navbar from "./components/Navbar"
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 const App = () => {
   return (
     <div className="relative min-h-screen w-screen overflow-x-hidden">
-      <Navbar/>
-      <Hero/>
-      <About/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
     </div>
-  )
+  );
 };
 
 export default App;
